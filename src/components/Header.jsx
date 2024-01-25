e7873f7fa37670502f090511288bb366a4cc01c1
Abdumalik <abdumaliksharipov@gmail.com>
2024-01-24 22:16:53 -0600

    feat: add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import logoImg from '../assets/logo.jpg';
+
+const Header = () => {
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src={logoImg} alt="A fresh burger " />
+        <h1>ReactBurger</h1>
+      </div>
+      <nav>
+        <button>Cart (0)</button>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
