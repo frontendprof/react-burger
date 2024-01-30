3d8cd7993791bd725adbf8df32a9e44cae0e35b9
Abdumalik <abdumaliksharipov@gmail.com>
2024-01-30 03:21:16 -0600

    feat: add custom http hook

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,23 +1,18 @@
-import { useState, useEffect } from 'react';
 import MealItem from './MealItem.jsx';
+import useHttp from '../hooks/useHttp.js';
+
+const requestConfig = {};
 
 const Meals = () => {
-  const [dataMeal, setDataMeal] = useState([]);
+  const {
+    data: dataMeal,
+    isLoading,
+    error,
+  } = useHttp('http://localhost:3000/meals', requestConfig, []);
 
-  useEffect(() => {
-    const fetchMeals = async () => {
-      const res = await fetch('http://localhost:3000/meals');
-
-      if (!res.ok) {
-        // ...
-      }
-
-      const data = await res.json();
-      setDataMeal(data);
-    };
-
-    fetchMeals();
-  }, []);
+  if (isLoading) {
+    return <p className="center">Data fetching in progress ...</p>;
+  }
   return (
     <ul id="meals">
       {dataMeal.map((item) => (
